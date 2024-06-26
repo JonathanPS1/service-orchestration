314a7b79ea98200e2868b2320ea84125dc2e67e4
GoTadashi <kevinat908@gmail.com>
2024-06-26 23:31:33 +0700

    update getter setter modal order

--- src/main/java/com/marketplace/marketplace/model/order.java
+++ src/main/java/com/marketplace/marketplace/model/order.java
@@ -15,4 +15,84 @@ public class order {
     private Double total;
     private Double pembayaran;
     private Double kembalian;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKode_transaksi() {
+        return kode_transaksi;
+    }
+
+    public void setKode_transaksi(String kode_transaksi) {
+        this.kode_transaksi = kode_transaksi;
+    }
+
+    public String getTanggal_pembelian() {
+        return tanggal_pembelian;
+    }
+
+    public void setTanggal_pembelian(String tanggal_pembelian) {
+        this.tanggal_pembelian = tanggal_pembelian;
+    }
+
+    public String getKode_pelanggan() {
+        return kode_pelanggan;
+    }
+
+    public void setKode_pelanggan(String kode_pelanggan) {
+        this.kode_pelanggan = kode_pelanggan;
+    }
+
+    public String getKode_barang() {
+        return kode_barang;
+    }
+
+    public void setKode_barang(String kode_barang) {
+        this.kode_barang = kode_barang;
+    }
+
+    public Integer getJumlah_barang() {
+        return jumlah_barang;
+    }
+
+    public void setJumlah_barang(Integer jumlah_barang) {
+        this.jumlah_barang = jumlah_barang;
+    }
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Double getPembayaran() {
+        return pembayaran;
+    }
+
+    public void setPembayaran(Double pembayaran) {
+        this.pembayaran = pembayaran;
+    }
+
+    public Double getKembalian() {
+        return kembalian;
+    }
+
+    public void setKembalian(Double kembalian) {
+        this.kembalian = kembalian;
+    }
 }
