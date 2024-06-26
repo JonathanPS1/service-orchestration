c427e4d9c8fae0cee387ac0aaa46651a866f3f72
Panca16 <672021010@student.uksw.edu>
2024-06-26 23:38:45 +0700

    Add model customer

--- src/main/java/com/marketplace/marketplace/model/customer.java
+++ src/main/java/com/marketplace/marketplace/model/customer.java
@@ -1,5 +1,45 @@
 package com.marketplace.marketplace.model;
 
+import jakarta.persistence.Id;
+
 public class customer {
+    @Id
+    private String id;
+    private String nama;
+    private String email;
+    private String alamat;
+    private String no_hp;
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getAlamat() {
+        return alamat;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    public String getNo_hp() {
+        return no_hp;
+    }
+    public void setNo_hp(String no_hp) {
+        this.no_hp = no_hp;
+    }
+
+
 
 }
